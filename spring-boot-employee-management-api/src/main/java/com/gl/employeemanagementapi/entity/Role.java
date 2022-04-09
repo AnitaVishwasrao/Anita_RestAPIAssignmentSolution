458b011ce26e0ad377fdb1f9ca8072dbcbb553e8
Anita Vishvasrao <vishwasrao.anita@yahoo.com>
2022-04-10 01:27:24 +0530

    Initial Commit

---
+++ spring-boot-employee-management-api/src/main/java/com/gl/employeemanagementapi/entity/Role.java
@@ -0,0 +1,43 @@
+package com.gl.employeemanagementapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "roles")
+public class Role {
+	@Id
+	@Column(name = "role_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long roleId;
+
+	@Column(name = "name")
+	private String roleName;
+
+	public long getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(long roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
